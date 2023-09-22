60961fe07fa4067f7620a35805b9618cd40d92ad
Stas Yakobov <stas@3os.org>
2023-09-22 17:13:39 +0300

    20230922.1713

--- content/config.mjs
+++ content/config.mjs
@@ -1,4 +1,11 @@
 const config = {
+  title: "dev.3os.org",
+  description: "This is a test site useing Floweshow and Obsidian",
+  author: "fire1ce",
+  domain: "dev.3os.org",
+  navLinks: [{ href: "/", name: "Home", href: "/about", name: "About" }],
   showToc: true,
+  showEditLink: true,
+  editLinkRoot: "https://github.com/fire1ce/dev.3os.org/edit/main/site",
 };
-export default {};
+export default config;
